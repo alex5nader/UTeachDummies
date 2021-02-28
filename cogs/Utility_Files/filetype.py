3a91416812d3ea3a4cd5fdbb6097c3ed149c5611
Huy Nguyen <73027756+huyaboo@users.noreply.github.com>
2021-02-28 03:08:05 -0600

    Add files via upload

---
+++ cogs/Utility_Files/filetype.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+PATH = os.getenv("FILE_PATH")
+
+fileType = namedtuple("fileType", "Type Mime")
+allTypes = []
+
+file = open(PATH + "Filetypes.txt", "r")
+lines = file.readlines()
+for line in lines:
+	data = line.split("\t")
+	type = data[0].strip()
+	mime = data[1].strip()
+	m = fileType(type, mime)
+	allTypes.append(m)
+file.close()
+
+def whatFile(filename): 
+	filename = filename.strip()
+	temp = filename.split(".")[1]
+	extension = temp.lower()
+
+
+	for i in range(len(allTypes)):
+		if allTypes[i].Type.strip() == extension.strip():
+			return allTypes[i].Mime.strip()
+	return "None"
+
+
+
