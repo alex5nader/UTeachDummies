c7c96af915ace3fafcbac8765f609362d9941ee7
Huy Nguyen <73027756+huyaboo@users.noreply.github.com>
2021-02-28 11:20:38 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
-import discord
+#Built-in Modules
 import os
+
+#Packages from pip install
+import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -12,11 +15,19 @@ def main():
 	# Help command: Update as needed
 	@client.command()
 	async def help(ctx):
-		await ctx.send(embed=discord.Embed(
+		help = discord.Embed(
 			color=4359413,
 			title='Help',
 			description='THE place for managing your UTD courses. Below is a list of commands.'
-		))
+		)
+		help.add_field(name = "search + <filename>", value = "Finds all files and folders with the inputted name.", inline = False)
+		help.add_field(name = "createcourse + <Course Name>", value = "Creates a new course folder.", inline = False)
+		help.add_field(name = "createprofessor + <Course Name>, <Professor Name>", value = "Creates a professor inside an existing course.", inline = False)
+		help.add_field(name = "upload + <Course Name>, <Professor Name", value = "Uploads a compatible file under 8MB to the desired professor.", inline = False)
+		help.add_field(name = "download + <Course Name>, <Professor Name>, <filename>", value = "Downloads desired file.", inline = False)
+		help.add_field(name = "delete + <Course Name>, <Professor Name>, <filename>", value = "Deletes desired file. Note: users cannot delete folders.", inline = False)
+		help.set_footer(text = "Note: filename includes the extension (e.g. '.txt', '.jpeg')", icon_url = help.Empty)
+		await ctx.send(embed=help)
 
 	# Loads commands from other python files
 	@client.command('load')
@@ -47,7 +58,6 @@ def main():
 
 	# Run bot
 	client.run(TOKEN)
-
 
 if __name__ == "__main__":
 	main()
