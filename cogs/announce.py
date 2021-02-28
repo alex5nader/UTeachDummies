82e9f256aab904af432723a530e9eec0408f758b
Megan Vu <75195965+MeganVu@users.noreply.github.com>
2021-02-28 09:32:39 -0600

    Scheduled announcements finished

--- cogs/announce.py
+++ cogs/announce.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from asyncio import TimeoutError
+import datetime
 
 class Announce (commands.Cog):
     
@@ -9,20 +10,25 @@ class Announce (commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Bot is online.')
+        print('Bot is online.') 
 
     @commands.command()
     async def announce(self, ctx):
+        # check to make sure that the message being stored is from the person who initiated the command
         def same_author_and_channel(message):
             return message.author == ctx.author and message.channel == ctx.channel
+
         msgChannel = ''
+        # store the message to be sent
         await ctx.send ('Type the announcement that you want to schedule')
         msg = await ctx.bot.wait_for('message', check=same_author_and_channel)
         while msgChannel == '':
+        # designate the channel where the message is sent
             await ctx.send('What channel would you like the announcement to be sent in?')
             try:
                 tempChannel = await ctx.bot.wait_for('message', check=same_author_and_channel, timeout = 60.0)
-                await ctx.send(tempChannel.raw_channel_mentions)
+                # await ctx.send(tempChannel.raw_channel_mentions)
+            # make sure that the user doesn't take too much time, so they don't accidentally activate this command later
             except TimeoutError:
                 await ctx.send('One minute has passed without a reply, cancelling setup.')
                 return
@@ -30,8 +36,19 @@ class Announce (commands.Cog):
                await ctx.send('You did not enter a valid channel name. Please try again.')
             else:
                 msgChannel = tempChannel.raw_channel_mentions[0]
-        await ctx.send(f'Your announcement will be sent in <#{msgChannel}>. What time would you like to send it?')
-
+        await ctx.send(f'Your announcement will be sent in <#{msgChannel}>. What day would you like to send it? Please enter in YYYY-MM-DD format.')
+        date_entry = await ctx.bot.wait_for('message', check=same_author_and_channel, timeout = 60.0)
+        date_string = date_entry.content
+        year, month, day = map(int, date_string.split('-'))
+        dateToMsg = datetime.date(year, month, day)
+        dateToMsg = dateToMsg.strftime("%m/%d/%y")
+        await ctx.send(f'Your announcement will be sent on {dateToMsg}. What time would you like to send it? Please enter in 24-hour HH:MM format')
+        time_entry = await ctx.bot.wait_for('message', check=same_author_and_channel, timeout = 60.0)
+        time_string = time_entry.content
+        hour, minute = map(int, time_string.split(':'))
+        timeToMsg = datetime.time(hour, minute)
+        timeToMsg = timeToMsg.strftime("%I:%M")
+        await ctx.send(f'Your announcement will be sent in <#{msgChannel}> at {timeToMsg} on {dateToMsg}.')
 
 def setup(client):
     client.add_cog(Announce(client))
